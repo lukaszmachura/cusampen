979a7504a4d90def07bbc108b85ecb6356978e25
LM <lukasz.machura@smcebi.edu.pl>
2018-09-12 12:30:02 +0200

    all vectors, SampEn

--- cs.cu
+++ cs.cu
@@ -50,19 +50,29 @@ bool is_equal(float a, float b, float eps)
 }
 
 __global__
-void findvec(float *base_vec, float *in, int *out)
+void findvec(float *base_vec, float *in, int *mvec, int *mplus1vec)
 {
   int index = blockIdx.x * blockDim.x + threadIdx.x;
   
-  int ok = true;
+  // check if vec of size m is similar
+  int m = true;
   for (int i = 0; i < d_m; i++){
     if (!is_equal(in[index + i], base_vec[i], d_r)){
-      ok = false;
+      m = false;
       break;
     }
   }
+  if (m) mvec[index] = 1;
 
-  if (ok) out[index] = 1;
+  // check if vec of size m + 1 is similar
+  int mplus1 = true;
+  for (int i = 0; i <= d_m; i++){ 
+    if (!is_equal(in[index + i], base_vec[i], d_r)){
+      mplus1 = false;
+      break;
+    }
+  }
+  if (mplus1) mplus1vec[index] = 1;
 }
 
 int load_data(char *fname, float *x)
@@ -97,8 +107,8 @@ int main(void)
 {
   int i;
   float *x;
-  int *idx;
-  int *matches;
+  int *mvec, *mplus1vec;
+  int *mmatches, *mplus1matches;
   float *base_vec, r;
   int m;
 
@@ -112,41 +122,65 @@ int main(void)
   int numBlocks = (N + blockSize - 1) / blockSize;
 
   // Allocate Unified Memory – accessible from CPU or GPU
-  gpuErrchk(cudaMallocManaged(&x, N*sizeof(float)));
-  gpuErrchk(cudaMallocManaged(&matches, sizeof(int)));
-  gpuErrchk(cudaMallocManaged(&idx, N*sizeof(int)));
+  gpuErrchk(cudaMallocManaged(&x, N * sizeof(float)));
+  gpuErrchk(cudaMallocManaged(&mmatches, sizeof(int)));
+  gpuErrchk(cudaMallocManaged(&mplus1matches, sizeof(int)));
+  gpuErrchk(cudaMallocManaged(&mvec, N * sizeof(int)));
+  gpuErrchk(cudaMallocManaged(&mplus1vec, N * sizeof(int)));
 
   // initialize data
   load_data(fname, x);
-  for (i = 0; i < N; i++) idx[i] = 0;
-  matches[0] = 0;
- 
+
   // Sampen algorithm initialisation
   m = 2;
   gpuErrchk(cudaMemcpyToSymbol(d_m, &m, sizeof(int), 0, cudaMemcpyHostToDevice));
   r = 0.2f;
   gpuErrchk(cudaMemcpyToSymbol(d_r, &r, sizeof(float), 0, cudaMemcpyHostToDevice));
 
-  // build base vec
-  gpuErrchk(cudaMallocManaged(&base_vec, m*sizeof(float)));
-  //for (i = 0; i < m; i++) base_vec[i] = i == 0 ? 1: -1;
-  for (i = 0; i < m; i++) base_vec[i] = x[i];
+  // space in shared mem for base vec (m + 1)
+  gpuErrchk(cudaMallocManaged(&base_vec, (m + 1) * sizeof(float)));
+  
+  // search for EACH possible base vec
+  int n_m = 0,
+      n_mplus1 = 0;
+  for (int ibv = 0; ibv < N - m - 1; ibv++){
+      // clean storage
+      for (i = 0; i < N; i++){ 
+          mvec[i] = 0;
+          mplus1vec[i] = 0;
+      }
+ 
+      // build temporary base vec of (m + 1) length
+      for (i = ibv; i <= m; i++) base_vec[i] = x[i];
 
-  // Run kernel on the GPU
-  // find matches
-  findvec<<<numBlocks, blockSize>>>(base_vec, x, idx);
-  gpuErrchk(cudaDeviceSynchronize());
+      // Run kernel on the GPU
+      // find matches for temporary vec
+      findvec<<<numBlocks, blockSize>>>(base_vec, x, mvec, mplus1vec);
+      gpuErrchk(cudaDeviceSynchronize());
 
-  //reduce
-  reduce<<<numBlocks, blockSize>>>(idx, matches);
-  gpuErrchk(cudaDeviceSynchronize());
+      //reduce
+      mmatches[0] = 0;
+      reduce<<<numBlocks, blockSize>>>(mvec, mmatches);
+      gpuErrchk(cudaDeviceSynchronize());
+      n_m += mmatches[0] - 1;
 
-  fprintf(stdout, "vector matches: %d\n", matches[0]);
+      mplus1matches[0] = 0;
+      reduce<<<numBlocks, blockSize>>>(mplus1vec, mplus1matches);
+      gpuErrchk(cudaDeviceSynchronize());
+      n_mplus1 += mplus1matches[0] - 1;
+  }//end of search
+
+  fprintf(stdout, "m vector matches: %d\n", n_m);
+  fprintf(stdout, "(m+1) vector matches: %d\n", n_mplus1);
+  fprintf(stdout, "ratio = n_{m+1}/n_m: %f\n", (float)n_mplus1/n_m);
+  fprintf(stdout, "SampEn = -ln(ratio): %f\n", -log((float)n_mplus1/n_m));
 
   // Free memory
   gpuErrchk(cudaFree(x));
-  gpuErrchk(cudaFree(matches));
-  gpuErrchk(cudaFree(idx));
+  gpuErrchk(cudaFree(mmatches));
+  gpuErrchk(cudaFree(mplus1matches));
+  gpuErrchk(cudaFree(mvec));
+  gpuErrchk(cudaFree(mplus1vec));
   
   return 0;
 }
